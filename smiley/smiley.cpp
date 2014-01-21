32ccb5dbb394621890a8fbb7fa1bac4686790383
mars <mars@cl1222.yappa.co.jp>
2014-01-21 17:20:24 +0900

    added smailey

---
+++ smiley/smiley.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Q. Write a C program to fill the display screen with smiley.
+// (C funny program)
+
+// Hint: ASCII value of Smiley Icon is 1.
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+  int x,y;
+  for(x=1, y=1; y <=1000; y++){
+    cout<< static_cast<char>(x);
+  }
+  return 0;
+}
